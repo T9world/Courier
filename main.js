fdbf8760e9442685432356ef0efd30a674509428
t9devworld <worldwidestar73@gmail.com>
2022-07-06 06:00:28 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,47 @@
+
+
+
+
+console.log("Hello World");
+console.log(1500);
+console.log(23.5);
+console.log(true);
+
+
+var world="t29";
+
+var world2="t100";
+
+console.log(world,world2);
+
+
+
+
+
+
+
+if(world === "t9"){
+    console.log("We are coming");
+}else if(world2 === "t10"){
+    console.log("Hello World");
+}else {
+    console.log("None of above")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
